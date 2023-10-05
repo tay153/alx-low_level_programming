1622dd72fe1c90a574c55d89dd3f7cad3856faea
tay153 <tay153@yahoo.com>
2023-10-05 11:24:13 -0700

    2 - 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Program that prints exactly a string using puts
+ * followed by a new line.
+ * Return: 0(success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
