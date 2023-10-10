a166c61ee40b5d40657f532a25d34558f88d060f
tay153 <tay153@yahoo.com>
2023-10-10 11:48:16 -0700

    alphabetical characters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetical char
+ * @c: ASCII character to be checked
+ * Return: 1 if c is a letter, otherwise 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
