63cccf7aaaf054da9985c8f6d148607fe1d483e6
tay153 <tay153@yahoo.com>
2023-10-13 08:38:06 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,3 +3,12 @@
  *  print_most_numbers - prints the numbers, from 0 to 9
  *  does not print 2 and 4
  */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	if (i != '2' && i != '4' && i <= '9')
+		_putchar(i);
+	i++;
+	_putchar('\n');
+}
